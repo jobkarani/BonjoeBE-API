be9d5555e4e23ff2067203d55d7261e7562ef306
Job Karani <jobkarani084j@gmail.com>
2022-12-18 22:43:14 +0300

    initial commit

---
+++ app/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+from django.urls import reverse
+from pyuploadcare.dj.models import ImageField
+# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def get_url(self):
+        return reverse('products_by_category', args=[self.slug])
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    image = ImageField( manual_crop="")
+    image2 = ImageField(blank=True, null=True, manual_crop="")
+    image3 = ImageField(blank=True,null=True, manual_crop="")
+    description = models.TextField(max_length=4000)
+    new_price = models.FloatField()
+    old_price = models.FloatField()
+    is_available = models.BooleanField(default = True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ('name',)
+
+    def get_url(self):
+        return reverse('product_detail', args=[self.category.slug, self.slug])
+
+    def __str__(self):
+        return self.name
