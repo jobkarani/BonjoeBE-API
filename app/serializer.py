7270c92ceb1a9da3725e1fdc3db541d360f7c71b
Job Karani <jobkarani084j@gmail.com>
2023-04-08 16:33:17 +0300

    fix: deployment

--- app/serializer.py
+++ app/serializer.py
@@ -16,9 +16,9 @@ class CategorySerializer(serializers.ModelSerializer):
         fields = ['id', 'name', 'slug','image', 'products']
 
 class PhoneSerializer(serializers.ModelSerializer):
-    category_name = serializers.CharField(source='phonecategory.name')
-    category_slug = serializers.CharField(source='phonecategory.slug')
-    category_id = serializers.IntegerField(source='phonecategory.id')
+    category_name = serializers.CharField(source='category.name')
+    category_slug = serializers.CharField(source='category.slug')
+    category_id = serializers.IntegerField(source='category.id')
     class Meta:
         model = Phones
         fields = ['id', 'name', 'slug', 'image', 'image2', 'image3', 'description','new_price', 'old_price', 'is_available','category_name','category_slug','category_id']
